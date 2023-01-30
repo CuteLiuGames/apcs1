e0fcab0769b7e521ffa9d09aee0cf5382f4813a0
CL-Games <tinahsieh2003@gmail.com>
2023-01-30 05:25:06 +0000

    a

---
+++ ext1-5.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    printf("A=>");
+    scanf("%d",&a);
+    printf("B=>");
+    scanf("%d",&b);
+    printf("A+B=%d\n", a+b);
+
+    return 0;
+}
