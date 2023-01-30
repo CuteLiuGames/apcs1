f53ab8dc62aebbb21ac0c81559e34bbe2e175293
CuteLiu Games <75060019+CuteLiuGames@users.noreply.github.com>
2023-01-30 11:12:14 +0800

    Create ext1-3.cpp

---
+++ ext1-3.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World Lucas, your tall is " << 1 << " m " << 70 << " cm " << endl;
+    printf("Hello World Lucas, your tall is %d m %d cm \n",1,70);
+    
+    printf(" hello Selena "); 
+     printf(" my height is %d m %d cm \n",1,80);
+     printf(" this building is %5d cm \n", 890);
+     printf(" this building is %5d cm \n", 7890);
+     printf(" this building is %5d cm \n", 67890);
+     printf(" this building is %5d cm \n", 567890);
+     printf(" this building is %5d cm \n", 1234567890);
+     printf(" pi = %f \n ", 3.1415926535897932384626433832795);
+    return 0;
+}
